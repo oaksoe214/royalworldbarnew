3b90594156cce3cf0c328dd7ff5adaee51e173e0
sandar89 <pooh.sandar@gmail.com>
2020-11-06 02:47:53 +0630

    test

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ app.get('/admin/updateroombooking/:doc_id', async function(req,res){
 app.post('/admin/updateroombooking', async function(req,res){
   console.log('REQ:', req.body);
   
-  let data={
+  let data = {
     name:req.body.name,
     phone:req.body.phone,
     email:req.body.email,
@@ -197,33 +197,16 @@ app.post('/admin/updateroombooking', async function(req,res){
     date:req.body.date,
     time:req.body.time,
     message:req.body.message,
-    name:req.body.status,
+    status:req.body.status,
     doc_id:req.body.doc_id,
-    ref:req.body.ref
+    ref:req.body.ref,
+    comment:req.body.comment
   }
+  
   db.collection('roombookings').doc(req.body.doc_id)
   .update(data).then(()=>{
-      res.redirect('/admin/roombooking');
+    res.redirect('/admin/roombookings');
   }).catch((err)=>console.log('ERROR:',error));
-
-  // let data = {
-  //   name:req.body.name,
-  //   phone:req.body.phone,
-  //   email:req.body.email,
-  //   room:req.body.room,
-  //   visit:req.body.visit,
-  //   date:req.body.date,
-  //   time:req.body.time,
-  //   message:req.body.message,
-  //   status:req.body.status,
-  //   doc_id:req.body.doc_id,
-  //   ref:req.body.ref
-  // }
-  
-  // db.collection('roombookings').doc(req.body.doc_id)
-  // .update(data).then(()=>{
-  //   res.redirect('/admin/roombookings');
-  // }).catch((err)=>console.log('ERROR:',error));
 
 });
 
