c0d6a957bd3b534a94583ccad2cde53feefa0378
sandar89 <pooh.sandar@gmail.com>
2020-12-05 23:01:41 +0630

    food-rom

--- app.js
+++ app.js
@@ -969,12 +969,12 @@ const confirmAppointment = (sender_psid) => {
  const confirmFoodOrder = (sender_psid) => {
   console.log('FOOD ORDER INFO',userInputs);
    let Summary = "foodorder:" + userInputs[user_id].foodorder + "\u000A";
-   Summary += "food:" + userInputs[user_id].food + "\u000A";
-   Summary += "date:" + userInputs[user_id].date + "\u000A";
-   Summary += "time:" + userInputs[user_id].time + "\u000A";
-   Summary += "name:" + userInputs[user_id].name + "\u000A";
-   Summary += "phone:" + userInputs[user_id].phone + "\u000A";
-   Summary += "email:" + userInputs[user_id].email + "\u000A";
+   // Summary += "food:" + userInputs[user_id].food + "\u000A";
+   // Summary += "date:" + userInputs[user_id].date + "\u000A";
+   // Summary += "time:" + userInputs[user_id].time + "\u000A";
+   // Summary += "name:" + userInputs[user_id].name + "\u000A";
+   // Summary += "phone:" + userInputs[user_id].phone + "\u000A";
+   // Summary += "email:" + userInputs[user_id].email + "\u000A";
    Summary += "message:" + userInputs[user_id].message + "\u000A";
    
   let response1 = {"text": Summary};
